d538164a041336e62524b21d08b718c8fa05cd6f
TaoYang-dev <TaoYang-dev@users.noreply.github.com>
2026-05-06 00:02:22 -0400

    Bust PWA cache for sync update

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "monthly-flow-v3";
+const CACHE_NAME = "monthly-flow-v4";
 const APP_ASSETS = [
   "./",
   "./index.html",
-  "./styles.css",
-  "./app.js",
-  "./manifest.webmanifest",
+  "./styles.css?v=4",
+  "./app.js?v=4",
+  "./manifest.webmanifest?v=4",
   "./icon.svg"
 ];
 
